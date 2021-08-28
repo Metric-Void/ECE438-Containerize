8aac66e53774aee4f7f5b13047b00c0a07d9a3d4
Metric-Void <metricvoidlx@gmail.com>
2021-08-28 14:44:38 -0500

    Expose container SSH

--- Start-Container.ps1
+++ Start-Container.ps1
@@ -1,16 +1,21 @@
 
-If($args.Length -eq 0) {
-    Write-Host "Container ID unspecified."
-    $id = Read-Host "Container Id"
+If($args.Count -eq 0) {
+    $id = [int](Read-Host "Container Id (Integer):")
 } Else {
-    $id = $args[0]
+    $id = [int]$args[0]
 }
+
+$id_str = $id.ToString()
+$ssh_portassign = ($id+43820).ToString()
 
 Write-Host "Starting Container $id"
 
-docker run -itd --name ece438-$id --hostname ece438-$id -v $pwd/repo:/repo metricvoid/ece438:20210827.b1
+$container_id = $(docker run -itd --name ece438-$id --hostname ece438-$id -p ${ssh_portassign}:22 -v $pwd/repo:/repo metricvoid/ece438:20210827.b1)
+
+$container_ip = $(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $container_id)
 
 Write-Host "Started Container with name ece348-$id"
-Write-Host "You can attach to it with Attach-Container"
-
+Write-Host "You can attach to it with Attach-Container ${id}"
+Write-Host "You can also access this container with SSH at localhost:${ssh_portassign}. Password for root is root"
+Write-Host "SSH commandline: ssh -lroot localhost -p ${ssh_portassign}"
 pause
